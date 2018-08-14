f77ad285d494090dbbb3a2b8c005fde772dad55e
Dylan Schroeder <schrodac@gmail.com>
2018-08-13 22:52:32 -0500

    code challenge 1 solution

---
+++ array_reverse/README.md
@@ -0,0 +1,8 @@
+# Reverse an Array
+Write a function to reverse an array
+
+## Challenge
+Can't use any built in methods like array.reverse
+
+## Solution
+![](assets/array_reverse.jpg)
---
+++ array_reverse/reverse.js
@@ -0,0 +1,8 @@
+function reverseArray(a) {
+  for(let i = 0, r = a.length-1; i < r; i++, r--)
+      [a[i], a[r]] = [a[r], a[i]];
+}
+let a = [1, 2, 3, 4, 5, 6]
+
+reverseArray(a);
+console.log(a);
