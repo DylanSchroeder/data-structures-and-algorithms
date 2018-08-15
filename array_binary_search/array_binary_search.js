b0d5bb037f232dce2854185f6842336833da9754
Dylan Schroeder <schrodac@gmail.com>
2018-08-15 16:36:15 -0500

    solution to code challenge 3

---
+++ array_binary_search/array_binary_search.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function binarySearch(array, searchKey) {
+  let max = array.length -1;
+  let min = 0;
+
+  var index;
+  var element;
+var count = 0;
+  while (min <= max && count++ < 100) {
+    index = Math.floor((min + max) / 2);
+    element = array[index];
+    console.log({ array, searchKey, count, min, index, max, element})
+
+    
+    if(element < searchKey) {
+      min = index + 1;
+    }
+    else if(element > searchKey) {
+      max = index - 1;
+    }
+    else {
+      return index;
+    }
+  }
+
+  return -1;
+}
+
+
+
+
+module.exports = binarySearch;
