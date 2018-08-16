a2a2ea959689923c9537410fd2270d0331a1bea0
Keith Dahlby <keith@newbo.co>
2018-08-16 09:34:32 -0500

    Add tests and debug helpers during review

--- array_binary_search/array_binary_search.js
+++ array_binary_search/array_binary_search.js
@@ -7,11 +7,15 @@ function binarySearch(array, searchKey) {
   var index;
   var element;
   var count = 0;
-  while (min <= max && count++ < 100) {
+  while (min <= max) {
+    // Guard against infinite loop!
+    if (count++ > 100) throw new Error('Infinite loop!');
+
     index = Math.floor((min + max) / 2);
     element = array[index];
-    
 
+    // It's fine to "cheat" to visualize how the algorithm is working
+    // console.log(array.slice(min, max+1));
     
     if(element < searchKey) {
       min = index + 1;
