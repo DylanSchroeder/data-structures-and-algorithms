f047e20c8d72d14a6e36f33f5418d587ecad923a
Dylan Schroeder <schrodac@gmail.com>
2018-08-17 20:51:29 -0500

    add tests for includes and for null

--- 05-linked-list/__tests__/linked-list.js
+++ 05-linked-list/__tests__/linked-list.js
@@ -10,5 +10,23 @@ describe('insert node', ()=> {
     expect(newList.head).not.toBe(null);
     expect(newList.head.value).toBe(5);
   });
+
+  describe('includes', ()=> {
+    it('can check if a value exists as a node value somewhere in the list', ()=>{
+      let listTest = new linkedList();
+      listTest.insertNode(10);
+      listTest.insertNode(20);
+      
+      expect(listTest.includes(10)).toBe(true);
+      expect(listTest.includes(15)).toBe(false);
+    });
+
+    describe('LinkedList', ()=>{
+      it('can check if there are any nodes in the list and return null if there are none', ()=>{
+        let noList = new linkedList();
+        expect(noList.head).toBe(null);
+      });
+    });
+  });
   
 });
