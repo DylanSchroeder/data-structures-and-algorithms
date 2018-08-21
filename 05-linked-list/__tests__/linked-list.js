4b8a2ec3f87ff9770dff6d675f4b1d0e793d5c48
Keith Dahlby <keith@newbo.co>
2018-08-21 10:05:05 -0500

    Restructure test

--- 05-linked-list/__tests__/linked-list.js
+++ 05-linked-list/__tests__/linked-list.js
@@ -2,13 +2,16 @@
 
 const linkedList = require('../linked-list');
 
-describe('insert node', ()=> {
-  it('adds a new node to the head of the list', ()=>{
-    let newList = new linkedList();
-    newList.insertNode(5);
+describe('linked-list', ()=> {
 
-    expect(newList.head).not.toBe(null);
-    expect(newList.head.value).toBe(5);
+  describe('insert node', ()=> {
+    it('adds a new node to the head of the list', ()=>{
+      let newList = new linkedList();
+      newList.insertNode(5);
+
+      expect(newList.head).not.toBe(null);
+      expect(newList.head.value).toBe(5);
+    });
   });
 
   describe('includes', ()=> {
